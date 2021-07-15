13c12d210752755344d9261ba8f83b1d7f03e53c
zetterburn <zetterburn@gmail.com>
2021-07-15 02:32:59 -0400

    Updates

---
+++ precache-manifest.2e86adf69fe34bf345b3bc72799968b6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fcd3d2353d1889bb826e6e77e21bc4",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "55bc410f9e663997e566",
+    "url": "/resume/static/css/2.1fbf2cff.chunk.css"
+  },
+  {
+    "revision": "212c4671bec4072116bb",
+    "url": "/resume/static/css/main.d8c18d63.chunk.css"
+  },
+  {
+    "revision": "55bc410f9e663997e566",
+    "url": "/resume/static/js/2.326c7d09.chunk.js"
+  },
+  {
+    "revision": "dfce09f1d02a634f128b38905e940a83",
+    "url": "/resume/static/js/2.326c7d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212c4671bec4072116bb",
+    "url": "/resume/static/js/main.2135c92f.chunk.js"
+  },
+  {
+    "revision": "6711e9d1eed4550e6cfd",
+    "url": "/resume/static/js/runtime-main.26d715af.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/resume/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/resume/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/resume/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/resume/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "f96135b7225a56ba37f98c1593f76e7b",
+    "url": "/resume/static/media/test.f96135b7.md"
+  }
+]);
